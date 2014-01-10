d68d9408a9540eaa48083b1af8ac1a826174dbbf
Josh Johnson <jjohnson@okta.com>
2014-01-10 12:40:48 -0800

    Start logging facility

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 var irc = require("irc");
 var cfg = require("./config.js");
 var fs = require("fs");
+var BotConsole;
 var moduleFiles = fs.readdirSync("modules/");
-var modules = [];
+var moduleConstructors = [];
+var modules = {};
 for(idx in moduleFiles) {
 	var moduleFile = moduleFiles[idx];
-	if(moduleFile.match(/.*\.js$/i)) modules.push(require("./modules/" + moduleFile));
+	if(moduleFile.match(/.*\.js$/i)) moduleConstructors.push(require("./modules/" + moduleFile));
 }
 
 var bot = new irc.Client(cfg.server, cfg.nick, {
 	channels: cfg.channels
 });
 
-for(idx in modules) {
-	console.log("Registering " + modules[idx].name);
-	new modules[idx](bot);
+for(idx in moduleConstructors) {
+	var modName = moduleConstructors[idx].name;
+	console.log("Registering " + modName);
+	modules[modName] = new moduleConstructors[idx](bot, cfg[modName]);
+	if(modName == "BotConsole") BotConsole = modules[modName];
 }
 
 // Silently discard IRC errors without crashing the bot
-bot.addListener("error", function(msg) {});
+//bot.addListener("error", function(msg) {});
 process.on("SIGHUP", function() {
 	bot.disconnect("Caught deadly SIGHUP");
 });
+
+function doLog(msg) {
+	if(BotConsole == undefined) console.log(msg);
+	else BotConsole.log(msg);
+}
--- modules/BotConsole.js
+++ modules/BotConsole.js
@@ -7,6 +7,8 @@
 module.exports = BotConsole;
 
 var bot;
+var that;
+var moment = require("moment");
 var readline = require("readline");
 var rl = readline.createInterface({
 	input: process.stdin,
@@ -16,12 +18,17 @@ var sigint = 0;
 
 function BotConsole(bawt) {
 	bot = bawt;
+	that = this;
 	bot.addListener("message", handleMessage);
-	console.log("Console loaded!");
+}
+
+BotConsole.prototype.log = function(msg, ts) {
+	if(ts == undefined) ts = true;
+	doLog(msg, ts);
 }
 
 function handleMessage(from, to, msg) {
-	doLog("[" + to + "] <" + from + "> " + msg);
+	that.log("[" + to + "] <" + from + "> " + msg);
 }
 
 function handleLine(line) {
@@ -39,9 +46,10 @@ function handleCmd(line) {
 	return;
 }
 
-function doLog(msg) {
+function doLog(msg, ts) {
+	var now = moment().format("YYYY-MM-DD HH:mm:ss");
 	rl.output.write("\x1b[2K\r");
-	console.log(msg);
+	console.log((ts ? now + " " : "") + msg);
 	rl.prompt(true);
 }
 
@@ -52,7 +60,7 @@ rl.on("line", function(l) { handleLine(l); rl.prompt() });
 rl.on("SIGINT", function() {
 	if(sigint < 1) {
 		++sigint;
-		doLog("Tap ^C again to kill the bot.");
+		doLog("Tap ^C again to kill the bot.", false);
 	} else {
 		console.log("\n\nCaught SIGINT... Bye!");
 		bot.disconnect("Caught SIGINT");
